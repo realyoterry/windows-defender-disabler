d6c6d5449ea7ff036099d27c8eb73beff28a6d63
Terry Kim <theterrykim@gmail.com>
2025-01-15 11:49:58 +1300

    feat: check for administrator

--- undo.bat
+++ undo.bat
@@ -8,6 +8,15 @@ if /i not "%proceed%"=="Y" (
     exit /b
 )
 
+:: Check for administrator privileges
+fsutil dirty query %systemdrive% >nul 2>&1
+if %errorlevel% neq 0 (
+    echo This script must be run as an administrator!
+    echo Right-click the .bat file and select "Run as administrator."
+    pause
+    exit /b
+)
+
 echo Reverting Windows Defender Settings...
 
 :: Define registry paths
