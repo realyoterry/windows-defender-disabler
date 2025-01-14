6185cbe97bcf8a6de26fabb86b0fe7fb1d35bd75
Terry Kim <theterrykim@gmail.com>
2025-01-15 11:43:21 +1300

    feat(disabler): add warning before run

--- disabler.bat
+++ disabler.bat
@@ -1,24 +1,33 @@
 @echo off
-echo Disabling Windows Defender Settings...
+echo WARNING: This will disable Windows Defender. Proceed? (Y/N)
+set /p proceed=
+if /i not "%proceed%"=="Y" (
+    echo Operation canceled.
+    exit /b
+)
 
-:: Navigate to Windows Defender Policies
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiVirus /t REG_DWORD /d 1 /f
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableSpecialRunningModes /t REG_DWORD /d 1 /f
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableRoutinelyTakingAction /t REG_DWORD /d 1 /f
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v ServiceKeepAlive /t REG_DWORD /d 1 /f
+:: Make a variable with the path to the Windows Defender key.
+echo Setting paths to registry keys...
+set DefenderPath=HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender
+set RealTimePath=%DefenderPath%\Real-Time Protection
+set SignaturePath=%DefenderPath%\Signature Update
+set SpynetPath=%DefenderPath%\Spynet
 
-:: Create Real-Time Protection Key and add DWORD values
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableBehaviourMonitoring /t REG_DWORD /d 1 /f
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableOnAccessProtection /t REG_DWORD /d 1 /f
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableRealTimeMonitoring /t REG_DWORD /d 1 /f
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableScanOnRealtimeEnable /t REG_DWORD /d 1 /f
+:: Apply Defender Policies
+reg add "%DefenderPath%" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
+if errorlevel 1 echo Failed to disable AntiSpyware
+reg add "%DefenderPath%" /v DisableAntiVirus /t REG_DWORD /d 1 /f
 
-:: Create Signature Update Key and add DWORD value
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Signature Update" /v ForceUpdateFromMU /t REG_DWORD /d 1 /f
+:: Real-Time Protection
+reg add "%RealTimePath%" /v DisableBehaviourMonitoring /t REG_DWORD /d 1 /f
+reg add "%RealTimePath%" /v DisableOnAccessProtection /t REG_DWORD /d 1 /f
+reg add "%RealTimePath%" /v DisableRealTimeMonitoring /t REG_DWORD /d 1 /f
 
-:: Create Spynet Key and add DWORD value
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet" /v DisableBlockAtFirstSeen /t REG_DWORD /d 1 /f
+:: Signature Update
+reg add "%SignaturePath%" /v ForceUpdateFromMU /t REG_DWORD /d 1 /f
+
+:: Spynet
+reg add "%SpynetPath%" /v DisableBlockAtFirstSeen /t REG_DWORD /d 1 /f
 
 echo Windows Defender settings have been configured.
 pause
